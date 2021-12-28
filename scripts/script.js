b8a242eb477ed76dd3d3ec43c37c73656d1f7436
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-28 17:20:39 -0300

    :sparkles: Added showCorrectLetter func

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const get = (str) => document.querySelector(str);
 const getAll = (str) => document.querySelectorAll(str);
 
 const alphabetList = get('.alphabet');
+const wordElement = get('.word');
 
 const words = [
   'item',
@@ -18,12 +19,20 @@ const pickRandomWord = () => words[Math.floor(Math.random() * words.length)];
 const randomWord = pickRandomWord();
 
 const renderWord = () => {
-  const wordElement = get('.word');
-
   randomWord.split('').forEach((ch) => {
     const li = `<li class="letter"><span class="hidden">${ch}</span></li>`;
     wordElement.insertAdjacentHTML('beforeend', li);
   });
+};
+
+const showCorrectLetter = (letter) => {
+  const { children } = wordElement;
+  [...children]
+    .filter((item) => item.textContent === letter)
+    .forEach((item) => {
+      const span = item;
+      span.innerHTML = `<span class="visible">${item.textContent}</span>`;
+    });
 };
 
 const updateWord = (event) => {
@@ -31,6 +40,10 @@ const updateWord = (event) => {
 
   if (target.classList.contains('digit')) {
     target.setAttribute('disabled', '');
+
+    if (randomWord.includes(target.textContent)) {
+      showCorrectLetter(target.textContent);
+    }
   }
 };
 
